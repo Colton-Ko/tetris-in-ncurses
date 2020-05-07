01d93bba482e11a8a4ce1ec9a5fe3ad720d1e4b9
EnLsGm <55735655+EnLsGm@users.noreply.github.com>
2020-05-07 14:08:44 +0800

    Adding reference
    Adding reference at the head of code.

--- include/tetris/blocks.h
+++ include/tetris/blocks.h
@@ -1,3 +1,26 @@
+/*
+	HKU ENGG 1340 Programming and technologies
+	Group 140 (2019-2020 2nd Semester)
+
+	AUTHOR
+	TAM CHUN KIT	(3035686554)
+	CHOW NAM FUNG	(35<x+>)
+
+	DATE
+	2020-05-07
+
+	FILENAME
+	blocks.cpp
+
+	REFERENCES
+	This piece has direct references to these sources
+		- https://youtu.be/8OK8_tHeCIA
+		- https://stackoverflow.com/questions/20326356/how-to-remove-all-the-occurrences-of-a-char-in-c-string
+
+	VIEW
+		Tabsize = 8
+		Indentation:	TAB
+*/
 #ifndef BLOCKS_H
 #define BLOCKS_H
 
