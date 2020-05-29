fc0de0b5b00471ad650c9ced10ba2e13b5ed3446
Kolton Ko <m4a79x@icloud.com>
2020-05-29 19:21:46 +0800

    Update debug.h

--- include/tetris/debug.h
+++ include/tetris/debug.h
@@ -1,10 +1,4 @@
 /*
-        HKU ENGG 1340 Programming and technologies
-        Group 140 (2019-2020 2nd Semester)
-
-        AUTHOR
-        TAM CHUN KIT            (3035686554)
-        CHOW NAM FUNG           (3035712767)
 
         DATE
         2020-05-07
@@ -15,6 +9,7 @@
         VIEW
                 Tabsize:        8
                 Indentation:    Space
+                
 */
 
 #ifndef DEBUG_H
